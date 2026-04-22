edfa8956d4056a400c48da214f61f74deacbd3b7
Abhishek Waghmare <abhishekwaghmare1996@gmail.com>
2026-04-22 17:07:50 +0530

    Migrations added and GameStoreContext added in Data folder for migrations

--- GameStore/GameStore.Api/Program.cs
+++ GameStore/GameStore.Api/Program.cs
@@ -1,11 +1,15 @@
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using GameStore.Api.Data;
 using GameStore.Api.Dtos;
 using GameStore.Api.EndPoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+
+var connString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
 
--- GameStore/GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore/GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9255ae004e68d3998a5edcb68d352a61824ca521")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08bf549fde2e74f1984e7360d19f1877402731f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
