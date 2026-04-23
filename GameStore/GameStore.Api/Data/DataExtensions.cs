4eb33891efe50b3507a816d32489a79cb4a2ad42
Abhishek Waghmare <abhishekwaghmare1996@gmail.com>
2026-04-23 12:44:06 +0530

    Data Seeding added

--- GameStore/GameStore.Api/Data/DataExtensions.cs
+++ GameStore/GameStore.Api/Data/DataExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using GameStore.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Data;
@@ -13,5 +14,33 @@ public static class DataExtensions
         var dbContext = scope.ServiceProvider
                         .GetRequiredService<GameStoreContext>();
         dbContext.Database.Migrate();
+    }
+
+    public static void AddGameStoreDb(this WebApplicationBuilder builder)
+    {
+        var connString = "Data Source=GameStore.db";
+        builder.Services.AddSqlite<GameStoreContext>(
+            connString,
+            // Here Data seeding is added to the database, it will run every time the application starts, but it will only add data if the Genres table is empty.
+            // Database seeding means inserting initial data into a database automatically when it is created or updated.
+            optionsAction: options => options.UseSeeding((context, _) =>
+            {
+                if (!context.Set<Genre>().Any())
+                {
+                    context.Set<Genre>().AddRange(
+                        new Genre { Name = "Fighting" },
+                        new Genre { Name = "RPG" },
+                        new Genre { Name = "Platformer" },
+                        new Genre { Name = "Racing" },
+                        new Genre { Name = "Sports" }
+                    );
+
+                    context.SaveChanges();
+                    // If SQLite explorer is not seen then use ctrl + shift + p 
+                    // and search for SQLite: Open Database and select the GameStore.db file in the project directory to view the database and its tables.
+                }
+            })
+
+        );
     }
 }
--- GameStore/GameStore.Api/Program.cs
+++ GameStore/GameStore.Api/Program.cs
@@ -1,15 +1,12 @@
 using FluentValidation;
-using FluentValidation.AspNetCore;
 using GameStore.Api.Data;
-using GameStore.Api.Dtos;
 using GameStore.Api.EndPoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
-var connString = "Data Source=GameStore.db";
-builder.Services.AddSqlite<GameStoreContext>(connString);
+builder.AddGameStoreDb();
 
 var app = builder.Build();
 
